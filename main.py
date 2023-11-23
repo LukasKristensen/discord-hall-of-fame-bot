ab61666897a0151769a2fbbadae26b8ed78d8d92
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-11-23 01:36:28 +0100

    Prevent duplicate messages

--- main.py
+++ main.py
@@ -7,9 +7,23 @@ YOUR_DEDICATED_CHANNEL_ID = 1177040595395547197
 # Open as text file
 bot_token = open("bot_token.txt", "r").read().splitlines()[0]
 
+# File to store sent message IDs
+file_path = 'sent_messages.txt'
+
+# Set to store sent message IDs
+sent_messages = set()
+
+# Load existing sent message IDs from the file
+try:
+    with open(file_path, 'r') as file:
+        sent_messages = {int(line.strip()) for line in file.readlines()}
+except FileNotFoundError:
+    pass
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -19,7 +33,7 @@ async def on_raw_reaction_add(payload):
 
     # Adjust these values according to your requirements
     target_channel_id = YOUR_DEDICATED_CHANNEL_ID
-    reaction_threshold = 1
+    reaction_threshold = 1  # Update the reaction threshold to 1
 
     if channel_id == target_channel_id:
         return  # Ignore reactions in the dedicated channel
@@ -28,16 +42,25 @@ async def on_raw_reaction_add(payload):
     message = await channel.fetch_message(message_id)
 
     # Check if the message has surpassed the reaction threshold
-    if len(message.reactions) >= reaction_threshold:
+    if len(message.reactions) >= reaction_threshold and message.id not in sent_messages:
         # Get the dedicated channel
         target_channel = bot.get_channel(target_channel_id)
 
-        # Send the message content and a link to the original message
+        # Find the first attachment (assuming it's an image or video)
+        attachment_url = message.attachments[0].url if message.attachments else None
+
+        # Send the message content, a link to the original message, and the media link
         await target_channel.send(
             f"Message with ID {message.id} in {channel.mention} has surpassed {reaction_threshold} reactions:\n"
             f"{message.content}\n"
+            f"Media: {attachment_url}\n"
             f"[Jump to Message]({message.jump_url})"
         )
+
+        # Add the sent message ID to the set and the file
+        sent_messages.add(message.id)
+        with open(file_path, 'a') as file:
+            file.write(f"{message.id}\n")
 
 # Run the bot with your token
 bot.run(bot_token)
