bbb6ebae4301c79f0b419fe7597406aa58a99ef9
Lukas Kristensen <lukadk00@gmail.com>
2024-09-05 15:58:23 +0200

    Update llm_msg.py

--- llm_msg.py
+++ llm_msg.py
@@ -14,95 +14,96 @@ def check_hof_msg(discord_msg: str, label: str = "confirm attendance"):
     return hof_result
 
 
-filter_messages = [
-    "Hey, vil du spille nogle spil i aften?",
-    "Hvor mange er klar til mødet?",
-    "Er der nogen der er klar til at spille Apex Legends?",
-    "Skal vi ramme virksomheden kl 20? Helst før",
-    "Skal vi ramme virksomheden kl 19?",
-    "Office kl 20?",
-    "Gutter, vi skal spille perfect heist i dag. Skal vi sige kl 20?",
-    "Skal vi sige kl 19:30 med perfect heist?",
-]
+if __name__ == '__main__':
+    filter_messages = [
+        "Hey, vil du spille nogle spil i aften?",
+        "Hvor mange er klar til mødet?",
+        "Er der nogen der er klar til at spille Apex Legends?",
+        "Skal vi ramme virksomheden kl 20? Helst før",
+        "Skal vi ramme virksomheden kl 19?",
+        "Office kl 20?",
+        "Gutter, vi skal spille perfect heist i dag. Skal vi sige kl 20?",
+        "Skal vi sige kl 19:30 med perfect heist?",
+    ]
 
-correct_messages = [
-    "Mig i dag med en Harboe Pilsner",
-    "-1000 aura",
-    ":joyyy:",
-    "Nånå, hvad har sådan en aften med fire fine piger kostet jer? :tf:",
-    "The god gamer gambit",
-    "Håber luffy er med i spillet",
-    "https://steamcommunity.com/sharedfiles/filedetails/?id=3262815200",
-]
+    correct_messages = [
+        "Mig i dag med en Harboe Pilsner",
+        "-1000 aura",
+        ":joyyy:",
+        "Nånå, hvad har sådan en aften med fire fine piger kostet jer? :tf:",
+        "The god gamer gambit",
+        "Håber luffy er med i spillet",
+        "https://steamcommunity.com/sharedfiles/filedetails/?id=3262815200",
+    ]
 
-labels = [
-    "play games",
-    "check readiness",
-    "check readiness for playing games",
-    "decide time or date",
-    "confirm attendance",
-]
+    labels = [
+        "play games",
+        "check readiness",
+        "check readiness for playing games",
+        "decide time or date",
+        "confirm attendance",
+    ]
 
-cutoff_threshold = 0.97
+    cutoff_threshold = 0.97
 
-labels_summarized = {}
+    labels_summarized = {}
 
-def llm_evaluate(label: str = "confirm attendance", plot_confusion_matrix = False):
-    print("\nEvaluating LLM for label:", label)
+    def llm_evaluate(label: str = "confirm attendance", plot_confusion_matrix = False):
+        print("\nEvaluating LLM for label:", label)
 
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
+        tp = 0
+        tn = 0
+        fp = 0
+        fn = 0
 
-    for message in filter_messages:
-        result = classifier(message, label)
+        for message in filter_messages:
+            result = classifier(message, label)
 
-        if any(score>cutoff_threshold for score in result['scores']):
-            print('\033[92m',result)
-            tp += 1
-        else:
-            print('\033[91m',result)
-            fn += 1
+            if any(score>cutoff_threshold for score in result['scores']):
+                print('\033[92m',result)
+                tp += 1
+            else:
+                print('\033[91m',result)
+                fn += 1
 
-    for message in correct_messages:
-        result = classifier(message, label)
+        for message in correct_messages:
+            result = classifier(message, label)
 
-        if any(score > cutoff_threshold for score in result['scores']):
-            print('\033[92m', result)
-            fp += 1
-        else:
-            print('\033[91m', result)
-            tn += 1
+            if any(score > cutoff_threshold for score in result['scores']):
+                print('\033[92m', result)
+                fp += 1
+            else:
+                print('\033[91m', result)
+                tn += 1
 
-    conf_matrix = np.array([[tn, fp],
-                            [fn, tp]])
+        conf_matrix = np.array([[tn, fp],
+                                [fn, tp]])
 
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    accuracy = (tp + tn) / (tp + tn + fp + fn)
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
 
-    print("\033[0m")  # Resetting color formatting
+        print("\033[0m")  # Resetting color formatting
 
-    print(conf_matrix)
+        print(conf_matrix)
 
-    labels_summarized[label] = {"Accuracy": accuracy, "Precision": precision, "Recall": recall}
+        labels_summarized[label] = {"Accuracy": accuracy, "Precision": precision, "Recall": recall}
 
-    if plot_confusion_matrix:
-        plt.figure(figsize=(6, 5))
-        plt.title(f'LLM with label: {label}')
-        sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
-                    xticklabels=['Predicted Negative', 'Predicted Positive'],
-                    yticklabels=['Actual Negative', 'Actual Positive'])
-        plt.title('Confusion Matrix')
-        plt.xlabel('Predicted Labels')
-        plt.ylabel('True Labels')
-        plt.show()
+        if plot_confusion_matrix:
+            plt.figure(figsize=(6, 5))
+            plt.title(f'LLM with label: {label}')
+            sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
+                        xticklabels=['Predicted Negative', 'Predicted Positive'],
+                        yticklabels=['Actual Negative', 'Actual Positive'])
+            plt.title('Confusion Matrix')
+            plt.xlabel('Predicted Labels')
+            plt.ylabel('True Labels')
+            plt.show()
 
 
-for candidate_label in labels:
-    llm_evaluate(candidate_label)
+    for candidate_label in labels:
+        llm_evaluate(candidate_label)
 
-for label_eval in labels_summarized:
-    print("Label:", label_eval, labels_summarized[label_eval])
+    for label_eval in labels_summarized:
+        print("Label:", label_eval, labels_summarized[label_eval])
 
