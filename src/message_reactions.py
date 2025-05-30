690d63b4c21ba65d3cfdd93041c30f42ab26abea
Lukas Kristensen <lukadk00@gmail.com>
2025-05-30 21:43:33 +0200

    Customizable calculation methods for reaction count

--- src/message_reactions.py
+++ src/message_reactions.py
@@ -2,7 +2,7 @@ import discord as discord
 import main
 
 
-def most_reactions(reactions: [discord.Reaction]) -> [discord.Reaction]:
+def most_reacted_emoji(reactions: [discord.Reaction]) -> [discord.Reaction]:
     custom_emoji_check_logic = main.db_client[str(reactions[0].message.guild.id)]["server_config"].find_one({})["custom_emoji_check_logic"]
     whited_listed_emojis = main.db_client[str(reactions[0].message.guild.id)]["server_config"].find_one({})["whitelisted_emojis"]
 
@@ -31,7 +31,32 @@ def most_reactions(reactions: [discord.Reaction]) -> [discord.Reaction]:
     return biggest
 
 
-async def reaction_count_without_author(message):
+def total_reaction_count(reactions: [discord.Reaction]) -> int:
+    custom_emoji_check_logic = main.db_client[str(reactions[0].message.guild.id)]["server_config"].find_one({})["custom_emoji_check_logic"]
+    whited_listed_emojis = main.db_client[str(reactions[0].message.guild.id)]["server_config"].find_one({})["whitelisted_emojis"]
+
+    if custom_emoji_check_logic and len(whited_listed_emojis) > 0:
+        corrected_reactions = []
+        for reaction in reactions:
+            if str(reaction.emoji) in str(whited_listed_emojis):
+                corrected_reactions.append(reaction)
+        reactions = corrected_reactions
+
+    return sum([reaction.count for reaction in reactions])
+
+
+def unique_reactor_count(reaction: discord.Reaction) -> int:
+    custom_emoji_check_logic = main.db_client[str(reaction.message.guild.id)]["server_config"].find_one({})["custom_emoji_check_logic"]
+    whited_listed_emojis = main.db_client[str(reaction.message.guild.id)]["server_config"].find_one({})["whitelisted_emojis"]
+
+    if custom_emoji_check_logic and len(whited_listed_emojis) > 0:
+        if str(reaction.emoji) not in str(whited_listed_emojis):
+            return 0
+
+    return len(set([user.id async for user in reaction.users()]))
+
+
+def most_reacted_emoji_from_message(message: discord.Message) -> [discord.Reaction]:
     max_reaction_count = 0
     server_includes_author_in_threshold = main.db_client[str(message.guild.id)]["server_config"].find_one({})["include_author_in_reaction_calculation"]
     custom_emoji_check_logic = main.db_client[str(message.guild.id)]["server_config"].find_one({})["custom_emoji_check_logic"]
@@ -57,3 +82,7 @@ async def reaction_count_without_author(message):
         max_reaction_count = react_count if react_count > max_reaction_count else max_reaction_count
 
     return max_reaction_count
+
+
+async def reaction_count(message):
+    return most_reacted_emoji_from_message(message)
